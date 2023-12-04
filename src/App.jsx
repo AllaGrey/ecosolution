1e04639fca37738c900473cf50a69de8a6101541
AllaGrey <111253692+AllaGrey@users.noreply.github.com>
2023-12-04 16:07:22 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { GlobalStyle } from './components/GlobalStyles/GlobalStyles';
 import { Container } from './App.styled';
 import { MainSection } from './components/MainSection/MainSection';
 import { AboutSection } from './components/AboutSection/AboutSection';
-import { CasesSection } from './components/CasesSection/CasesSection';
+import { CasesSection } from './components/CasesSectoin/CasesSection';
 import { FAQSection } from './components/FAQSection/FAQSection';
 import { ContactSection } from './components/ContactSection/ContactSection';
 import { Footer } from './components/Footer/Footer';
